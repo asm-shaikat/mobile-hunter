b79d69c8dd8e0cc98372c0b2f73d81ef15f74505
asm-shaikat <asmshaikat1952@gmail.com>
2024-07-07 01:40:08 +0600

    showing details of specfic mobile

--- public/js/api.js
+++ public/js/api.js
@@ -16,6 +16,7 @@ const displayData = (phones) => {
         mobileCard.innerHTML = '<p class="text-gray-600">No results found</p>';
         return;
     }
+    
     phones.forEach(phone => {
         const card = document.createElement('div');
         card.className = 'card bg-white rounded-lg shadow-lg overflow-hidden w-72';
@@ -27,12 +28,16 @@ const displayData = (phones) => {
                 <h2 class="text-lg font-semibold">${phone.phone_name}</h2>
                 <p class="mt-2 text-gray-600">${phone.brand}</p>
                 <div class="mt-4">
-                    <button class="btn btn-primary w-full">Buy Now</button>
+                    <button class="btn btn-primary w-full m-1" onclick="showPopup('${phone.slug}')">Details</button>
+                    <button class="btn btn-primary w-full m-1">Buy Now</button>
                 </div>
             </div>
         `;
         mobileCard.appendChild(card);
     });
+    // Hide loading screen and show content
+    document.getElementById('loading-section').style.display = 'none';
+    document.getElementById('content-section').style.display = 'block';
 }
 
 function search(){
@@ -42,7 +47,34 @@ function search(){
     } else {
         loadIphone(searchText);
     }
+     // Show loading screen and hide content
+     document.getElementById('loading-section').style.display = 'flex';
+     document.getElementById('content-section').style.display = 'none';
 }
+
+function showPopup(slug){
+    mobileDetails.showModal();
+    showDetails(slug);
+}
+ const showDetails = async (slug) =>{
+    const details = await fetch(`https://openapi.programming-hero.com/api/phone/${slug}`);
+    const result = await details.json();
+    const res = result.data;
+    console.log(res);
+    const content = document.getElementById('mobileDetails');
+    content.innerHTML = `
+    <div class="modal-box">
+              <form method="dialog">
+                <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+              </form>
+              <img src="${res.image}" alt="${res.phone_name}" class="w-32 object-cover" />
+              <h3 class="text-lg font-bold">Name: ${res.name}</h3>
+              <p>Chipset: ${res.mainFeatures.chipSet?res.mainFeatures.chipSet:"Not Available"}</p>
+              <p>Display Size: ${res.mainFeatures.displaySize?res.mainFeatures.displaySize:"Not Available"}</p>
+              <p class="py-4">Press ESC key or click on ✕ button to close</p>
+            </div>
+    `
+ }
 document.addEventListener('DOMContentLoaded', () => {
     loadIphone(); 
 });
